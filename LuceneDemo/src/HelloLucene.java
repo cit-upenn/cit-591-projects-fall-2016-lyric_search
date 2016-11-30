1ddfa3096ee9949263236f1822e17d73b15baf65
Jon <huang.jonathanw@gmail.com>
2016-11-30 15:53:30 -0500

    got a working basic lucene demo with search and indexing!

--- LuceneDemo/src/HelloLucene.java
+++ LuceneDemo/src/HelloLucene.java
@@ -14,39 +14,55 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.Version;
 
 import java.io.IOException;
 
 public class HelloLucene {
-  public static void main(String[] args) throws IOException, ParseException {
+	public static void main(String[] args) throws IOException, ParseException {
     // 0. Specify the analyzer for tokenizing text.
     //    The same analyzer should be used for indexing and searching
     StandardAnalyzer analyzer = new StandardAnalyzer();
 
     // 1. create the index
-    Directory index = new RAMDirectory();
-
+    // to store index on disk use: 
+//    Directory directory = FSDirectory.open("~Development/EclipseWorkSpace/HomeWorkSix/cit-591-projects-fall-2016-lyric_search/LuceneDemo");
+    Directory directory = new RAMDirectory();
     IndexWriterConfig config = new IndexWriterConfig();
-
-    IndexWriter w = new IndexWriter(index, config);
-    addDoc(w, "Lucene in Action", "193398817");
-    addDoc(w, "Lucene for Dummies", "55320055Z");
-    addDoc(w, "Managing Gigabytes", "55063554A");
-    addDoc(w, "The Art of Computer Science", "9900333X");
-    w.close();
+    IndexWriter iWriter = new IndexWriter(directory, config);
+   
+//  need some while loop here to go through the file and create each document
+    Document doc = new Document();
+    String title = "We Will Rock You";
+    String artist = "Queen";
+    String lyrics = "Buddy you're a boy make a big noise"
+    		+ " Playin' in the street gonna be a big man some day"
+    		+ "You got mud on yo' face"
+    		+ "You big disgrace"
+    		+ "Kickin' your can all over the place Singin'";
+//    String lyrics = "happy birthday is the lyrics to this song boy! ";
+    
+    // adds each field to the document
+    doc.add(new TextField("title", title, Field.Store.YES));
+    doc.add(new TextField("artist", artist, Field.Store.YES));
+    doc.add(new TextField("lyrics", lyrics, Field.Store.NO));
+    //write the document to the index
+    iWriter.addDocument(doc);
+    
+    iWriter.close();
 
     // 2. query
-    String querystr = args.length > 0 ? args[0] : "lucee~ for dums~";	// ~ at end of each word for fuzzy search
+    String querystr = args.length > 0 ? args[0] : "kick~ all oer the plac~";	// ~ at end of each word for fuzzy search
 
     // the "title" arg specifies the default field to use
     // when no field is explicitly specified in the query.
-    Query q = new QueryParser("title", analyzer).parse(querystr);
+    Query q = new QueryParser("lyrics", analyzer).parse(querystr);
 
     // 3. search
     int hitsPerPage = 10;
-    IndexReader reader = DirectoryReader.open(index);
+    IndexReader reader = DirectoryReader.open(directory);
     IndexSearcher searcher = new IndexSearcher(reader);
     TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
     searcher.search(q, collector);
@@ -57,20 +73,21 @@ public class HelloLucene {
     for(int i=0;i<hits.length;++i) {
       int docId = hits[i].doc;
       Document d = searcher.doc(docId);
-      System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
+      System.out.println((i + 1) + ". " + d.get("artist") + "\t" + d.get("title"));
     }
 
     // reader can only be closed when there
     // is no need to access the documents any more.
     reader.close();
+    directory.close();
   }
 
-  private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
-    Document doc = new Document();
-    doc.add(new TextField("title", title, Field.Store.YES));
-
-    // use a string field for isbn because we don't want it tokenized
-    doc.add(new StringField("isbn", isbn, Field.Store.YES));
-    w.addDocument(doc);
-  }
+//	private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
+//		Document doc = new Document();
+//		doc.add(new TextField("title", title, Field.Store.YES));
+//
+//		// use a string field for isbn because we don't want it tokenized
+//		doc.add(new StringField("isbn", isbn, Field.Store.YES));
+//		w.addDocument(doc);
+//	}
 }
