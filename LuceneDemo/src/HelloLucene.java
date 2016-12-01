335e908c7baf55a44abeac51df4ae90d11cd5602
Jon <huang.jonathanw@gmail.com>
2016-12-01 14:53:37 -0500

    added functionality for searching directly from a pre-indexed file; index files added

--- LuceneDemo/src/HelloLucene.java
+++ LuceneDemo/src/HelloLucene.java
@@ -24,8 +24,11 @@ import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.queryparser.classic.Token;
 import org.apache.lucene.search.FuzzyQuery;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MultiPhraseQuery;
+import org.apache.lucene.search.PhraseQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopScoreDocCollector;
@@ -42,11 +45,11 @@ import java.util.Scanner;
 
 public class HelloLucene {
 	public static void main(String[] args) throws IOException, ParseException {
+
 		FileReader file = new FileReader("song_list.txt");
 		/*
-		 * =============================
-		 * stop list with this code
-		 * stop list code FileReader stoplist = new FileReader("stop_list.txt");
+		 * ============================= stop list with this code stop list code
+		 * FileReader stoplist = new FileReader("stop_list.txt");
 		 * 
 		 * CharArraySet stopset = new CharArraySet(stoplist.getFile().size(),
 		 * true); for (String words : stoplist.getFile()) { stopset.add(words);
@@ -54,27 +57,30 @@ public class HelloLucene {
 		 */
 
 		SongData songData = new SongData(file);
-		
+
 		// 0. Specify the analyzer for tokenizing text.
 		// The same analyzer should be used for indexing and searching
 		/*
 		 * StandardAnalyzer analyzer = new StandardAnalyzer(stopset);
 		 */
 		StandardAnalyzer analyzer = new StandardAnalyzer();
-		
+		// EnglishAnalyzer analyzer = new EnglishAnalyzer();
+
 		// 1. create the index
 		// to store index on disk use:
-		/*======================================
-		 * store index in path with this code
+		/*
+		 * ====================================== store index in path with this
+		 * code
 		 * 
 		 * Path docDir = Paths.get("index"); Directory directory =
 		 * FSDirectory.open(docDir);
 		 */
+		// Path docDir = Paths.get("index"); Directory directory =
+		// FSDirectory.open(docDir);
 		Directory directory = new RAMDirectory();
 		IndexWriterConfig config = new IndexWriterConfig();
 		IndexWriter iWriter = new IndexWriter(directory, config);
-		
-		
+
 		// need some while loop here to go through the file and create docs
 		for (Integer key : songData.getKeySet()) {
 			Document doc = new Document();
@@ -82,7 +88,7 @@ public class HelloLucene {
 			String album = songData.getAlbum(key);
 			String artist = songData.getArtist(key);
 			String lyrics = songData.getLyrics(key);
-			System.out.println(songData.getLyrics(key));
+			// System.out.println(songData.getLyrics(key));
 			// adds each field to the document
 			doc.add(new TextField("title", title, Field.Store.YES));
 			doc.add(new TextField("album", album, Field.Store.YES));
@@ -96,8 +102,15 @@ public class HelloLucene {
 
 		Scanner in = new Scanner(System.in);
 		boolean loop = true;
+		/*
+		 * to read from a pre-indexed file directly Path docDir =
+		 * Paths.get("index"); Directory directory = FSDirectory.open(docDir);
+		 * StandardAnalyzer analyzer = new StandardAnalyzer();
+		 */
+
 		IndexReader reader = DirectoryReader.open(directory);
-		
+		IndexSearcher searcher = new IndexSearcher(reader);
+
 		while (loop) {
 			System.out.println("search some lyrics or (q)uit: ");
 			String input = in.nextLine();
@@ -106,6 +119,7 @@ public class HelloLucene {
 				System.out.println();
 				System.out.println();
 				System.out.println("See you next time!");
+				in.close();
 				System.exit(1);
 			}
 
@@ -114,29 +128,36 @@ public class HelloLucene {
 			String querystr = args.length > 0 ? args[0] : input;
 			// the "title" arg specifies the default field to use
 			// when no field is explicitly specified in the query.
-		    /*
-		     * test code for stemmming using tokenstreams?
-		     */
-			TokenStream tokenstream = analyzer.tokenStream("lyrics", querystr);
-		    tokenstream = new LowerCaseFilter(tokenstream);
-		    tokenstream = new StopFilter(tokenstream, StopAnalyzer.ENGLISH_STOP_WORDS_SET);
-		    tokenstream = new PorterStemFilter(tokenstream);
-		    OffsetAttribute offsetAttribute = tokenstream.addAttribute(OffsetAttribute.class);
-		    CharTermAttribute charTermAttribute = tokenstream.addAttribute(CharTermAttribute.class);
-		    
-		    tokenstream.reset();
-		    while (tokenstream.incrementToken()) {
-		    	querystr = charTermAttribute.toString();
-		    }
-		    tokenstream.end();
-		    tokenstream.close();
-		    
+
+			/*
+			 * test code for stemmming using tokenstreams?
+			 * 
+			 * TokenStream tokenstream = analyzer.tokenStream("lyrics",
+			 * querystr); tokenstream = new LowerCaseFilter(tokenstream);
+			 * tokenstream = new StopFilter(tokenstream,
+			 * StopAnalyzer.ENGLISH_STOP_WORDS_SET); tokenstream = new
+			 * PorterStemFilter(tokenstream); OffsetAttribute offsetAttribute =
+			 * tokenstream.addAttribute(OffsetAttribute.class);
+			 * CharTermAttribute charTermAttribute =
+			 * tokenstream.addAttribute(CharTermAttribute.class);
+			 * 
+			 * tokenstream.reset(); while (tokenstream.incrementToken()) {
+			 * querystr = charTermAttribute.toString(); } tokenstream.end();
+			 * tokenstream.close();
+			 */
+
 			Query q = new QueryParser("lyrics", analyzer).parse(querystr);
+
+			// Query q = new MultiFieldQueryParser(fields,
+			// analyzer).parse(querystr);
+
 			// 3. search
 			int hitsPerPage = 5;
 			// copied the reader to outside the loop
 			// IndexReader reader = DirectoryReader.open(directory);
-			IndexSearcher searcher = new IndexSearcher(reader);
+			// copied searcher to outside of loop to not create it multiple
+			// times
+			// IndexSearcher searcher = new IndexSearcher(reader);
 			TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
 			searcher.search(q, collector);
 			ScoreDoc[] hits = collector.topDocs().scoreDocs;
@@ -145,7 +166,7 @@ public class HelloLucene {
 			System.out.println();
 			System.out.println("=====================================================================================");
 			System.out.println("searching... " + querystr);
-
+			System.out.println();
 			System.out.println("Found " + hits.length + " hits.");
 			for (int i = 0; i < hits.length; ++i) {
 				int docId = hits[i].doc;
@@ -156,6 +177,7 @@ public class HelloLucene {
 		}
 		// reader can only be closed when there
 		// is no need to access the documents any more.
+		in.close();
 		reader.close();
 		directory.close();
 	}
