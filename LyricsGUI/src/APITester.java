5dfdfbbdb85c333f986013d1a4eccaea4a8566c8
Saurav <saurav924@gmail.com>
2016-12-02 02:50:34 -0500

    updated youtube api caller and tester

--- LyricsGUI/src/APITester.java
+++ LyricsGUI/src/APITester.java
@@ -1,9 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class APITester {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		
+		YoutubeAPICaller yt = new YoutubeAPICaller();
+		
+		String searchInput = "where+it's+at";
+		String searchUrl = yt.buildUrl(searchInput);
+		String searchResponse = yt.doApiCall(searchUrl);
+		System.out.println(searchResponse);
+		HashMap <String, String> ID = yt.extractIdsFromResponse(searchResponse);
+		String videoId = "";
+		for(String element : ID.keySet()){
+			if(element.contains(searchInput.replaceAll("+", " "))){
+				videoId = ID.get(element);
+				System.out.println(videoId);
+				break;
+			}
+		}
 
 	}
-
+	
 }
