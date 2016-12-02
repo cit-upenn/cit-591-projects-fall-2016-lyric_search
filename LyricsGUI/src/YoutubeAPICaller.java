18d3cbcaf99cb2c5e3f9d84cbfe8473bd19a0883
Saurav <saurav924@gmail.com>
2016-12-02 00:45:53 -0500

    gui initial template

---
+++ LyricsGUI/src/YoutubeAPICaller.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.net.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class YoutubeAPICaller {
+	private final static String API_BASE = "https://www.googleapis.com/youtube/v3/";
+	private final static String MY_KEY = "AIzaSyDvolSDCsRWRnKmxPU3WujTXhAJHjoP0lE";
+	
+	
+
+}
