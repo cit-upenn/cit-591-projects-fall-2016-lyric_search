41e954c256dbc855f200583faf7b52e7720ea21f
Jon <huang.jonathanw@gmail.com>
2016-12-18 21:55:27 -0500

    finished search tests

--- Lyric_Search/Tests/Lucene_SearchTest.java
+++ Lyric_Search/Tests/Lucene_SearchTest.java
@@ -1,21 +1,47 @@
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.HashMap;
 
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.junit.Before;
 import org.junit.Test;
 
 public class Lucene_SearchTest {
-
+	
+	private Lucene_Search search;
+	HashMap<Integer, Song> results;
+	
+	@Before
+	public void setup() throws IOException {
+		search = new Lucene_Search();
+		results = new HashMap<>();
+	}
+	
 	@Test
-	public void test() {
+	public void testSearchIsNotNull() throws ParseException, IOException {
 		Lucene_Search search = null;
 		try {
 			search = new Lucene_Search();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		search.search(userQuery)
+		results = search.search("invite me");
+		assertNotNull("results is not empty", results);
+	}
+	
+	@Test
+	public void testSearch() throws ParseException, IOException {
+		HashMap<Integer, Song> results = search.search("invite me");
+		assertNotNull("results is not empty", results);
+	}
+	
+	@Test 
+	public void testClose() throws IOException, ParseException {
+		search.close();
+		results = search.search("invite me");
+//		assertExpected()
+		assertEquals("results should be null", results);
 	}
 
 }
